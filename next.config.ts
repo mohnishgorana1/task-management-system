5467992368536d5b360c3022d02c91a8764ed6c0
Mohnish Gorana <mohnishgorana1@gmail.com>
2025-10-02 10:38:07 +0530

    config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignores ESLint errors during build
   },
 };
 
